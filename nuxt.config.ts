48159548235a7cb5c458e9552f6a7a0f4f9e4d3a
Kat <shedidthatio22@gmail.com>
2023-02-13 17:53:18 -0500

    added tailwindcss module and meta info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: ['@nuxtjs/tailwindcss'],
 
+    app: {
+        head: {
+            title: 'Baby Shower Invitation',
+            meta: [
+                { name: 'description', content: 'You\'re invited to a baby shower, Hooray!' }
+            ],
+            link: [
+                {
+                    rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+                }
+            ]
+        }
+    }
 })
